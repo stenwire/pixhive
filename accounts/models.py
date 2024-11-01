7a87d3bc97c95a403712ac53b610b3c98b0b010a
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2024-11-01 04:16:12 +0100

    change field, fix syntax

--- accounts/models.py
+++ accounts/models.py
@@ -1,31 +1,36 @@
-from django.db import models
-from authentication.models import CustomUser
-from utils.models import TrackObjectStateMixin
 from django.conf import settings
+from django.db import models
+
+from utils.models import TrackObjectStateMixin
 
 # Create your models here.
 
+
 class Hiver(TrackObjectStateMixin):
-  user = models.ForeignKey(
+    user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name='hiver'
+        related_name="hiver",
     )
-  instagram = models.URLField(verbose_name='instagram', blank=True, null=True)
-  twitter = models.URLField(verbose_name='twitter', blank=True, null=True)
+    instagram = models.URLField(
+        verbose_name="instagram", blank=True, null=True
+    )
+    twitter = models.URLField(verbose_name="twitter", blank=True, null=True)
 
-  def __str__(self):
+    def __str__(self):
         return self.user.email
 
+
 class Collector(TrackObjectStateMixin):
-  user = models.ForeignKey(
+    user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name='collector'
+        related_name="collector",
     )
-  # email = models.EmailField(verbose_name='collector_email', unique=True)
-  # collection_id = models.ForeignKey("Collection", on_delete=models.CASCADE, related_name='collector')
-  # pass_code = models.CharField(max_length=6)
+    # email = models.EmailField(verbose_name='collector_email', unique=True)
+    # collection_id = models.ForeignKey(
+    # "Collection", on_delete=models.CASCADE, related_name='collector')
+    # pass_code = models.CharField(max_length=6)
 
-  def __str__(self):
-    return self.user.email
+    def __str__(self):
+        return self.user.email
